6aa4e9e0c5a218c9569a6fc4c502953920769a77
elisayadarola <elisayadarola@hotmail.com>
2022-03-07 14:57:01 +0100

    updated 2nd page

---
+++ index.js
@@ -0,0 +1,29 @@
+/*function createParagraph(){
+    const para = document.createElement('p');
+    para.textContent = "You just clicked";
+    return document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll('button');
+
+for (const button of buttons) {
+    button.addEventListener('click', createParagraph);
+}
+*/
+/*function createParagraph() {
+    const para = document.createElement('p');
+    para.textContent = 'You clicked the button!';
+    document.body.appendChild(para);
+  }
+  
+  const buttons = document.querySelectorAll('button');
+  
+  for (const button of buttons) {
+    button.addEventListener('click', createParagraph);
+  }
+  function createDiv(){
+    const ele = document.createElement('div');
+    ele.textContent = "Soy un div";
+    return document.body.appendChild(ele);
+}
+createDiv();*/
